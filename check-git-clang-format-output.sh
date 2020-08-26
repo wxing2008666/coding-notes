f6acfaa320126f07bb6364cda08aac95e172cffa
wxing2008666 <wxing2008666@126.com>
2020-08-26 15:17:30 +0800

    modify

--- check-git-clang-format-output.sh
+++ check-git-clang-format-output.sh
@@ -13,17 +13,17 @@ if git rev-parse --verify HEAD >/dev/null 2>&1; then
   fi
   origin_commit=$(echo "$GITHUB_CONTEXT" | jq -r '.event.pull_request.base.sha')
   current_commit=$(echo "$GITHUB_CONTEXT" | jq -r '.event.pull_request.head.sha')
-  echo $base_commit
   echo $origin_commit
-  echo "Running clang-format against parent commit $base_commit, and $origin_commit"
+  echo $current_commit
+  echo "Running clang-format against parent commit $origin_commit, and $current_commit"
 else
   #base_commit="$(git merge-base "${TRAVIS_BRANCH}" HEAD)"
-  base_commit=4b825dc642cb6eb9a060e54bf8d69288fbee4904
   origin_commit=4b825dc642cb6eb9a060e54bf8d69288fbee4904
-  echo "Running clang-format against branch $base_commit, with hash $(git rev-parse "$base_commit")"
+  current_commit=4b825dc642cb6eb9a060e54bf8d69288fbee4904
+  echo "Running clang-format against branch $current_commit, with hash $(git rev-parse "$current_commit")"
 fi
 #exclude_regex="(.*thirdparty/|.*redismodule.h|.*.java|.*.jsx?|.*.tsx?)"
-output="$(./git-clang-format --binary /usr/bin/clang-format --commit "$base_commit" "$origin_commit" --diff)"
+output="$(./git-clang-format --binary /usr/bin/clang-format --commit "$current_commit" "$origin_commit" --diff)"
 if [ "$output" = "no modified files to format" ] || [ "$output" = "clang-format did not modify any files" ] ; then
   echo "clang-format passed."
   exit 0
