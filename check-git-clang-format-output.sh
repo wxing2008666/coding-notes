715f3305a9addf2fbd461025dbc4d01dd52cae01
wxing2008666 <wxing2008666@126.com>
2020-08-26 17:36:10 +0800

    dev test

--- check-git-clang-format-output.sh
+++ check-git-clang-format-output.sh
@@ -17,8 +17,6 @@ if git rev-parse --verify HEAD >/dev/null 2>&1; then
   echo $origin_commit
   echo $current_commit
   echo "$GITHUB_SHA"
-  other_commit=`git rev-parse --verify HEAD^`
-  echo $other_commit
   echo "Running clang-format against parent commit $origin_commit, and $current_commit"
 else
   #base_commit="$(git merge-base "${TRAVIS_BRANCH}" HEAD)"
@@ -27,7 +25,7 @@ else
   echo "Running clang-format against branch $current_commit, with hash $(git rev-parse "$current_commit")"
 fi
 #exclude_regex="(.*thirdparty/|.*redismodule.h|.*.java|.*.jsx?|.*.tsx?)"
-output="$(./git-clang-format --binary clang-format --commit "$other_commit" "$origin_commit" --diff)"
+output="$(./git-clang-format --binary clang-format --commit "$origin_commit" --diff)"
 if [ "$output" = "no modified files to format" ] || [ "$output" = "clang-format did not modify any files" ] ; then
   echo "clang-format passed."
   exit 0
