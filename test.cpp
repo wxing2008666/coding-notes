77ca7c751627917c7201bcdb90cba8f554bb8fa6
wxing2008666 <wxing2008666@126.com>
2020-08-27 17:34:17 +0800

    test

--- test.cpp
+++ test.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main() {
-    cout << "hello, world!" << endl;
-    cout<<"hello"   <<   endl;
-    cout<<"test"<<   endl;
-    return 0;
+  cout << "hello, world!" << endl;
+  cout << "hello" << endl;
+  cout << "test" << endl;
+  return 0;
 }
