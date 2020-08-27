188ed488dbaa2dbd98ad1152707e744504cb1ec1
wxing2008666 <wxing2008666@126.com>
2020-08-27 11:53:43 +0800

    test

--- test.cpp
+++ test.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() 
 {
              cout << "hello, world!" << endl;
-	     cout<<"hello"   <<   endl;
+	        cout<<"hello"   <<   endl;
 	            cout<<"test"<<   endl;
   return 0;
 }
