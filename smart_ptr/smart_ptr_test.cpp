4bc1c449947a9ae6603a936ddc9019a73eb92c02
wxing2008666 <wxing2008666@126.com>
2020-11-07 19:18:29 +0800

    add smart_ptr test

---
+++ smart_ptr/smart_ptr_test.cpp
@@ -0,0 +1,44 @@
+﻿// smart_ptr_test.cpp : 用于测试智能指针的使用以及检测是否有内存泄露的问题
+//
+
+#include <iostream>
+#include <memory>
+
+// add for memory leak test
+// _CrtSetDbgFlag在程序退出时调用 _CrtDumpMemoryLeaks()
+#include <crtdbg.h>
+inline void enable_memory_leak_check() {
+    _CrtSetDbgFlag(_CrtSetDbgFlag(_CRTDBG_REPORT_FLAG) | _CRTDBG_LEAK_CHECK_DF);
+}
+
+// when use new operator, can print filename and line num
+#ifdef _DEBUG
+#define new new (_NORMAL_BLOCK, __FILE__, __LINE__)
+#endif
+
+// add test code
+class B;
+class A {
+public:
+    std::shared_ptr<B> p_b_;
+};
+
+class B {
+public:
+    //std::weak_ptr<A> p_a_;
+    std::shared_ptr<A> p_a_;
+};
+
+int main() {
+    enable_memory_leak_check();
+
+    auto a = std::make_shared<A>();
+    a->p_b_ = std::make_shared<B>();
+    a->p_b_->p_a_ = a;
+
+    //int* c = new int();
+
+    //_CrtDumpMemoryLeaks();
+
+    return 0;
+}
