9330137cb68cdb7606b5c1c6f115f61b7095054b
wxing2008666 <wxing2008666@126.com>
2022-02-19 17:54:55 +0800

    modify

--- rust/my_redis/src/main.rs
+++ rust/my_redis/src/main.rs
@@ -1,5 +1,7 @@
 use tokio::net::{TcpListener, TcpStream};
 use mini_redis::{Connection, Frame};
+use mini_redis::Command::{self, Get, Set};
+use std::collections::HashMap;
 
 #[tokio::main]
 async fn main() {
@@ -7,17 +9,33 @@ async fn main() {
 
     loop {
         let (socket, _) = listener.accept().await.unwrap();
-        process(socket).await;
+
+        tokio::spawn(async move {
+            process(socket).await;
+        });
     }
 }
 
 async fn process(socket: TcpStream) {
+    let mut db = HashMap::new();
     let mut connection = Connection::new(socket);
 
-    if let Some(frame) = connection.read_frame().await.unwrap() {
-        println!("get req from redis-cli:{:?}", frame);
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        let response = match Command::from_frame(frame).unwrap() {
+            Set(cmd) => {
+                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                Frame::Simple("OK".to_string())
+            },
+            Get(cmd) => {
+                if let Some(value) = db.get(cmd.key()) {
+                    Frame::Bulk(value.clone().into())
+                } else {
+                    Frame::Null
+                }
+            },
+            cmd => panic!("unimplement cmd:{:?}", cmd),
+        };
 
-        let response = Frame::Error("nothing".to_string());
         connection.write_frame(&response).await.unwrap();
     }
 }
