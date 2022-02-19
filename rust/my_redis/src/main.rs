932021372df3935b0805ac751679de7148f6fff6
wxing2008666 <wxing2008666@126.com>
2022-02-19 17:19:39 +0800

    add server

--- rust/my_redis/src/main.rs
+++ rust/my_redis/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::net::{TcpListener, TcpStream};
+use mini_redis::{Connection, Frame};
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+        process(socket).await;
+    }
+}
+
+async fn process(socket: TcpStream) {
+    let mut connection = Connection::new(socket);
+
+    if let Some(frame) = connection.read_frame().await.unwrap() {
+        println!("get req from redis-cli:{:?}", frame);
+
+        let response = Frame::Error("nothing".to_string());
+        connection.write_frame(&response).await.unwrap();
+    }
 }
