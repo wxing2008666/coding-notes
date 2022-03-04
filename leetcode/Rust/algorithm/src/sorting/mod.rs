40e8d0390a4dbe26cc3a00b1d870446f83cd1cd5
wxing2008666 <wxing2008666@126.com>
2022-03-04 13:53:52 +0800

    add selection sort

--- leetcode/Rust/algorithm/src/sorting/mod.rs
+++ leetcode/Rust/algorithm/src/sorting/mod.rs
@@ -1,7 +1,9 @@
 mod bubble_sort;
 mod bubble_sort_v2;
 mod quick_sort;
+mod selection_sort;
 
 pub use self::bubble_sort::bubble_sort;
 pub use self::bubble_sort_v2::bubble_sort as bubble_sort_v2;
 pub use self::quick_sort::quick_sort;
+pub use self::selection_sort::selection_sort;
---
+++ leetcode/Rust/algorithm/src/sorting/selection_sort.rs
@@ -0,0 +1,3 @@
+pub fn selection_sort<T: PartialOrd>(arr: &mut [T]) {
+    //
+}
