94e36e09c803aad600707646ded2c9919e8efd6a
wxing2008666 <wxing2008666@126.com>
2022-03-05 16:14:43 +0800

    add insert sort

--- leetcode/Rust/algorithm/src/sorting/mod.rs
+++ leetcode/Rust/algorithm/src/sorting/mod.rs
@@ -2,8 +2,10 @@ mod bubble_sort;
 mod bubble_sort_v2;
 mod quick_sort;
 mod selection_sort;
+mod insert_sort;
 
 pub use self::bubble_sort::bubble_sort;
 pub use self::bubble_sort_v2::bubble_sort as bubble_sort_v2;
 pub use self::quick_sort::quick_sort;
 pub use self::selection_sort::selection_sort;
+pub use self::insert_sort::insert_sort;
