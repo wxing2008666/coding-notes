bffa3674770612b94623a3d5bb28c2c8ca2b904b
wxing2008666 <wxing2008666@126.com>
2022-03-09 22:12:11 +0800

    modify

--- leetcode/Rust/algorithm/src/problem_0002_add_two_numbers/add_two_nums.rs
+++ leetcode/Rust/algorithm/src/problem_0002_add_two_numbers/add_two_nums.rs
@@ -4,7 +4,25 @@ pub struct Solution;
 
 impl Solution {
     pub fn add_two_nums(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-        Some(Box::new(ListNode::new(2)))
+        let mut head = ListNode::new(0);
+        let mut cur = &mut head.next;
+        let (mut x, mut y) = (l1, l2);
+        let mut upper = 0;
+        let node_val = |node: &Option<Box<ListNode>>| node.as_ref().map_or(0, |x| x.val);
+        let node_next = |node: Option<Box<ListNode>>| node.map_or(None, |node| node.next);
+        while x.is_some() || y.is_some() || upper == 1 {
+            let sum = node_val(&x) + node_val(&y) + upper;
+            upper = 0;
+            if sum >= 10 {
+                upper = 1;
+            }
+            let node = ListNode::new(sum % 10);
+            *cur = Some(Box::new(node));
+            cur = &mut cur.as_mut().unwrap().next;
+            x = node_next(x);
+            y = node_next(y);
+        }
+        return head.next;
     }
 }
 
--- leetcode/Rust/algorithm/src/test_utilities.rs
+++ leetcode/Rust/algorithm/src/test_utilities.rs
@@ -1,10 +1,10 @@
-use crate::data_structure::{ListNode, TreeNode};
-use std::cell::RefCell;
-use std::cmp::Ordering;
-use std::collections::VecDeque;
-use std::fmt::Debug;
+use crate::data_structure::{ListNode};
+// use std::rc::Rc;
+// use std::cell::RefCell;
+// use std::cmp::Ordering;
+// use std::collections::VecDeque;
+// use std::fmt::Debug;
 use std::iter;
-use std::rc::Rc;
 
 pub fn iter_list(list: &Option<Box<ListNode>>) -> impl Iterator<Item = &i32> {
     iter::successors(list.as_deref(), |node| node.next.as_deref()).map(|node| &node.val)
