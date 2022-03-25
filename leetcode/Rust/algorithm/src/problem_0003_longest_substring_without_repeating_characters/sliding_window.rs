87b9526f71ae7aeb1ecef2ab3113ac04b8b3d9aa
wxing2008666 <wxing2008666@126.com>
2022-03-25 13:16:52 +0800

    fix 0003

--- leetcode/Rust/algorithm/src/problem_0003_longest_substring_without_repeating_characters/sliding_window.rs
+++ leetcode/Rust/algorithm/src/problem_0003_longest_substring_without_repeating_characters/sliding_window.rs
@@ -19,7 +19,24 @@ use std::mem;
 
 impl Solution {
     pub fn length_longest_substring(s: String) -> i32 {
-        3
+        const INVALID_INDEX: isize = -1;
+        let s = s.into_bytes();
+        let mut result = 0;
+        let mut start = 0;
+        let mut byte_locations = [INVALID_INDEX; 256];
+
+        for (i, byte) in s.into_iter().enumerate() {
+            let i = i as isize;
+            let old_index = mem::replace(&mut byte_locations[byte as usize], i);
+
+            if old_index < start {
+                result = result.max(i - start + 1);
+            } else {
+                start = old_index + 1;
+            }
+        }
+
+        result as _
     }
 }
 
@@ -33,6 +50,6 @@ impl super::Solution for Solution {
 mod tests {
     #[test]
     fn test_problem_0003() {
-        //
+        super::super::tests::run::<super::Solution>();
     }
 }
