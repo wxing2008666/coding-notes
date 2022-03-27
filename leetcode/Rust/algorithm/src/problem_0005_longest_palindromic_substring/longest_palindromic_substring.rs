31f48a185b0d1130ae89480f8067941909cf083e
wxing2008666 <wxing2008666@126.com>
2022-03-27 22:13:04 +0800

    fix

--- leetcode/Rust/algorithm/src/problem_0005_longest_palindromic_substring/longest_palindromic_substring.rs
+++ leetcode/Rust/algorithm/src/problem_0005_longest_palindromic_substring/longest_palindromic_substring.rs
@@ -16,7 +16,34 @@ pub struct Solution;
 
 impl Solution {
     pub fn longest_palindromic(s: String) -> String {
-        "test".to_string()
+        let s = s.into_bytes();
+        let mut max_left = 0;
+        let mut max_length = 0;
+
+        let mut update_result = |mut i: usize, mut j: usize| {
+            while let (Some(lhs), Some(rhs)) = (s.get(i), s.get(j)) {
+                if lhs == rhs {
+                    i = i.wrapping_sub(1);
+                    j += 1;
+                } else {
+                    break;
+                }
+            }
+
+            let length = j.wrapping_sub(i) - 1;
+
+            if length > max_length {
+                max_left = i.wrapping_add(1);
+                max_length = length;
+            }
+        };
+
+        for i in 0..s.len() {
+            update_result(i.wrapping_sub(1), i + 1);
+            update_result(i, i + 1);
+        }
+
+        String::from_utf8(s[max_left..max_left + max_length].to_vec()).unwrap()
     }
 }
 
