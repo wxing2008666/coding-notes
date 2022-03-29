d02a5c12e7c9271a844be130b14a8080a6d4dc86
wxing2008666 <wxing2008666@126.com>
2022-03-29 13:30:32 +0800

    add problem 0011

--- leetcode/Rust/algorithm/src/lib.rs
+++ leetcode/Rust/algorithm/src/lib.rs
@@ -9,6 +9,7 @@ pub mod problem_0003_longest_substring_without_repeating_characters;
 pub mod problem_0004_median_of_two_sorted_arrays;
 pub mod problem_0005_longest_palindromic_substring;
 pub mod problem_0006_zigzag_conversion;
+pub mod problem_0011;
 
 // #[cfg(test)]
 mod test_utilities;
