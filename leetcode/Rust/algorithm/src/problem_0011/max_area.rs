268cb0ab6bf76b39f32204ff2d9ba1f3fc157eb9
wxing2008666 <wxing2008666@126.com>
2022-03-30 13:26:58 +0800

    fix 0011

--- leetcode/Rust/algorithm/src/problem_0011/max_area.rs
+++ leetcode/Rust/algorithm/src/problem_0011/max_area.rs
@@ -17,7 +17,36 @@ pub struct Solution;
 
 impl Solution {
     pub fn max_area(height: Vec<i32>) -> i32 {
-        1
+        let mut result = 0;
+        let (mut head, rest) = height.split_first().unwrap();
+        let (mut tail, mut body) = rest.split_last().unwrap();
+        let mut length = rest.len() as i32;
+
+        loop {
+            if head < tail {
+                result = result.max(length * head);
+
+                if let Some((new_head, new_body)) = body.split_first() {
+                    head = new_head;
+                    body = new_body;
+                } else {
+                    break;
+                }
+            } else {
+                result = result.max(length * tail);
+
+                if let Some((new_tail, new_body)) = body.split_last() {
+                    tail = new_tail;
+                    body = new_body;
+                } else {
+                    break;
+                }
+            }
+
+            length -= 1;
+        }
+
+        result
     }
 }
 
