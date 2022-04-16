02edd653b53f5222e1e431a3c48a753a59e5b30f
wxing2008666 <wxing2008666@126.com>
2022-04-16 22:45:52 +0800

    add iterator for v2

--- rust/recipes/lists/src/second.rs
+++ rust/recipes/lists/src/second.rs
@@ -41,6 +41,10 @@ impl<T> List<T> {
             &mut node.elem
         })
     }
+
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
 }
 
 impl<T> Drop for List<T> {
@@ -55,6 +59,16 @@ impl<T> Drop for List<T> {
         }
     }
 }
+
+pub struct IntoIter<T>(List<T>);
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
+    }
+}
+
 
 #[cfg(test)]
 mod test {
@@ -105,5 +119,17 @@ mod test {
 
         assert_eq!(list.peek(), Some(&42));
         assert_eq!(list.pop(), Some(42));
+    }
+
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+        list.push(1); list.push(2); list.push(3);
+
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), None);
     }
 }
